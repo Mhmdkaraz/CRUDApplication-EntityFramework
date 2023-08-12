4af8f34a73c4534043c5b7a104ae077e2c08bdee
Mhmdkaraz <mohamedkaraz22@gmail.com>
2023-08-12 16:27:57 +0300

    Implemented IPersonsRepository in PersonsRepository

---
+++ Repositories/PersonsRepository.cs
@@ -0,0 +1,57 @@
+﻿using Entities;
+using Microsoft.EntityFrameworkCore;
+using RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories {
+    public class PersonsRepository : IPersonsRepository {
+        private readonly ApplicationDbContext _db;
+        public PersonsRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public async Task<Person> AddPerson(Person person) {
+            _db.Add(person);
+            await _db.SaveChangesAsync();
+            return person;
+        }
+
+        public async Task<bool> DeletePersonByPersonId(Guid personId) {
+            _db.Persons.RemoveRange(_db.Persons.Where(temp => temp.PersonId == personId));
+            int rowsDeleted = await _db.SaveChangesAsync();
+            return rowsDeleted > 0;
+        }
+
+        public async Task<List<Person>> GetAllPersons() {
+            return await _db.Persons.Include("Country").ToListAsync();
+        }
+
+        public async Task<List<Person>> GetFilteredPersons(Expression<Func<Person, bool>> predicate) {
+            return await _db.Persons.Include("Country").Where(predicate).ToListAsync();
+        }
+
+        public async Task<Person?> GetPersonByPersonId(Guid personId) {
+           return await _db.Persons.Include("Country").FirstOrDefaultAsync(temp=>temp.PersonId == personId);
+        }
+
+        public async Task<Person> UpdatePerson(Person person) {
+            Person? matchingPerson = await _db.Persons.FirstOrDefaultAsync(temp => temp.PersonId == person.PersonId);
+            if (matchingPerson == null)
+                return person;
+            matchingPerson.PersonName = person.PersonName;
+            matchingPerson.Gender = person.Gender;
+            matchingPerson.Address = person.Address;
+            matchingPerson.CountryId = person.CountryId;
+            matchingPerson.DateOfBirth = person.DateOfBirth;
+            matchingPerson.ReceiveNewsLetters = person.ReceiveNewsLetters;
+
+            int countUpdated = await _db.SaveChangesAsync();
+            return matchingPerson;
+        }
+    }
+}
