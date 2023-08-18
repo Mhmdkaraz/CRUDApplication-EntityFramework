cefd7bb378a5a04113a71f6a8ee779a1d98afd4d
Mhmdkaraz <mohamedkaraz22@gmail.com>
2023-08-18 14:09:36 +0300

    Modified ResponseHeaderActionFilter to use IAsyncActionFilter

--- CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
+++ CRUDExample/Filters/ActionFilters/ResponseHeaderActionFilter.cs
@@ -1,33 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace CRUDExample.Filters.ActionFilters {
-    public class ResponseHeaderActionFilter : IActionFilter, IOrderedFilter {
+    public class ResponseHeaderActionFilter : IAsyncActionFilter, IOrderedFilter {
         private readonly ILogger<ResponseHeaderActionFilter> _logger;
-        private readonly string Key;
-        private readonly string Value;
+        private readonly string _key;
+        private readonly string _value;
 
         public int Order { get; set; }
 
 
         public ResponseHeaderActionFilter(ILogger<ResponseHeaderActionFilter> logger, string key, string value, int order) {
             _logger = logger;
-            Key = key;
-            Value = value;
+            _key = key;
+            _value = value;
             Order = order;
         }
 
-
-        //After
-        public void OnActionExecuted(ActionExecutedContext context) {
-            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuted));
-
-            context.HttpContext.Response.Headers[Key] = Value;
-        }
-
-
-        //Before
-        public void OnActionExecuting(ActionExecutingContext context) {
-            _logger.LogInformation("{FilterName}.{MethodName} method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecuting));
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next) {
+            _logger.LogInformation("{FilterName}.{MethodName} before method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
+            await next();//calls the subsequent filter or action method
+            _logger.LogInformation("{FilterName}.{MethodName} after method", nameof(ResponseHeaderActionFilter), nameof(OnActionExecutionAsync));
+            context.HttpContext.Response.Headers[_key] = _value;
         }
     }
 }
