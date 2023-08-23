669cb4c1aeaf11f88e29d2183ac181e46eaeeb76
Mhmdkaraz <mohamedkaraz22@gmail.com>
2023-08-23 09:49:51 +0300

    Added Class library for custom exceptions

---
+++ Exceptions/InvalidPersonIdException.cs
@@ -0,0 +1,13 @@
+﻿namespace Exceptions {
+    public class InvalidPersonIdException : ArgumentException {
+        public InvalidPersonIdException()
+        {
+        }
+        public InvalidPersonIdException(string? message) : base(message) {
+
+        }
+        public InvalidPersonIdException(string? message, Exception? innerException)
+        { 
+        }
+    }
+}
